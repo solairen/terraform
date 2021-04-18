117f3ad0124ecb146d21b6ac1bb8363e2770f026
Michał O <michal.oleszek@outlook.com>
2021-04-18 23:50:23 +0200

    updated workflows and terraform files

--- linode/linode_data.tf
+++ linode/linode_data.tf
@@ -1,37 +1,45 @@
 variable "token" {
     description = "Token to Linode cloud provider."
-    default = ""
+    type = string
     sensitive = true
 }
 
 variable "instance_name" {
     description = "Linode instance name."
-    default = ""
+    type = string
 }
 
 variable "os_system" {
     description = "Linode instance operating system."
+    type = string
     default = "linode/ubuntu20.04"
 }
 
 variable "region" {
     description = "Linode region."
+    type = string
     default = "eu-central"
 }
 
 variable "type" {
     description = "Linode instance type."
+    type = string
     default = "g6-nanode-1"
 }
 
 variable "ssh_keys" {
     description = "SSH keys to login to Linode instance."
-    default = ""
+    type = string
     sensitive = true
 }
 
 variable "root_pass" {
     description = "Root password for Linode instance."
-    default = ""
+    type = string
     sensitive = true
+}
+
+variable "tags" {
+    description = "Create a tag"
+    type = string
 }
