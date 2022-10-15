fdee36e0fd063038c30b961311cd7f92879e6781
Michał O <moleszek@duck.com>
2022-10-16 00:28:28 +0200

    Fixed problems from terraform-lint

--- aws/credentials.tf
+++ aws/credentials.tf
@@ -1,11 +1,11 @@
 variable "aws_access" {
   description = "AWS Access Key."
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 variable "aws_secret" {
   description = "AWS Secret Key."
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
